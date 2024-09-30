fcf802b4339e23a868224d04ad20d7c9b2345ba4
Sharon Zheng <sharzheng@gmail.com>
2024-09-29 22:07:09 -0400

    add preview img

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,25 @@
 import {
   Links,
   Meta,
+  MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
 import './tailwind.css';
 
+export const meta: MetaFunction = () => {
+  return [
+    { charSet: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { property: 'og:image', content: '/preview.png' },
+  ];
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
